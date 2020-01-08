60a4647939d69d89541b9782b318ce15c42a87e4
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 10:14:15 +0700

    Update MainActivity.kt

--- RetrofitBasic/app/src/main/java/com/derahh/retrofitbasic/MainActivity.kt
+++ RetrofitBasic/app/src/main/java/com/derahh/retrofitbasic/MainActivity.kt
@@ -2,11 +2,25 @@ package com.derahh.retrofitbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.derahh.retrofitbasic.adapter.UserAdapter
+import com.derahh.retrofitbasic.model.User
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val users = mutableListOf<User>()
+        for(i in 0..100) {
+            users.add(User("Dedi", "Rahmansah", "dedir2@gmail.com"))
+        }
+
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = UserAdapter(users)
+        }
     }
 }
