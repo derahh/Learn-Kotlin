62622913e6de5c1e3ee05cc4b7947604704bc8c9
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 14:53:11 +0700

    Update MainActivity.java

--- RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/MainActivity.java
+++ RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/MainActivity.java
@@ -1,14 +1,58 @@
 package com.derahh.retrofitbasicjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.derahh.retrofitbasicjava.Service.APIService;
+import com.derahh.retrofitbasicjava.adapter.UserAdapter;
+import com.derahh.retrofitbasicjava.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        APIService api = retrofit.create(APIService.class);
+
+        api.fetchAllUser().enqueue(new Callback<List<User>>() {
+            @Override
+            public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                showData(response.body());
+                Log.d("dedi", "" + response.body().get(0).getUsername());
+            }
+
+            @Override
+            public void onFailure(Call<List<User>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void showData(List<User> users) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new UserAdapter(users));
     }
 }
