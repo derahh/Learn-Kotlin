765a5f66033a31f3c3727ea016ef655415030eb0
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 14:18:37 +0700

    Update User.java

--- RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/model/User.java
+++ RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/model/User.java
@@ -1,4 +1,27 @@
 package com.derahh.retrofitbasicjava.model;
 
 public class User {
+    private String username;
+    private String name;
+
+    public User(String username, String name) {
+        this.username = username;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
