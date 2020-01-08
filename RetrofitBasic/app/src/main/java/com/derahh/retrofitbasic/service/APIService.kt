e6c5bc832acd62f3d696cc01c346b855fce2b0ac
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 11:10:09 +0700

    Update APIService.kt

--- RetrofitBasic/app/src/main/java/com/derahh/retrofitbasic/service/APIService.kt
+++ RetrofitBasic/app/src/main/java/com/derahh/retrofitbasic/service/APIService.kt
@@ -1,2 +1,11 @@
 package com.derahh.retrofitbasic.service
 
+import com.derahh.retrofitbasic.model.User
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface APIService {
+
+    @GET("/users")
+    fun fetchAllUsers(): Call<List<User>>
+}
