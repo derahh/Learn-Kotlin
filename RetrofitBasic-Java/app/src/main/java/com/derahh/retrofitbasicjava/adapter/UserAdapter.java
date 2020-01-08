6a4d9ea909e4259f6c6a78204ef9491a86057389
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 14:38:13 +0700

    Create UserAdapter.java

---
+++ RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/adapter/UserAdapter.java
@@ -0,0 +1,55 @@
+package com.derahh.retrofitbasicjava.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.derahh.retrofitbasicjava.R;
+import com.derahh.retrofitbasicjava.model.User;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private List<User> users;
+
+    public UserAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.userName.setText(user.getUsername());
+        holder.name.setText(user.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView userName;
+        private TextView name;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.txt_first_name);
+            name = itemView.findViewById(R.id.txt_last_name);
+        }
+    }
+}
