6f7c3debf2d47507453c936bc9d6eee25853de23
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 09:18:39 +0700

    Update MainActivity.java

--- Retrofit Basic/app/src/main/java/com/derahh/retrofitbasic/MainActivity.java
+++ Retrofit Basic/app/src/main/java/com/derahh/retrofitbasic/MainActivity.java
@@ -1,14 +1,37 @@
 package com.derahh.retrofitbasic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import com.derahh.retrofitbasic.adapter.CustomAdapter;
+import com.derahh.retrofitbasic.model.PhotoData;
+import com.derahh.retrofitbasic.services.ApiClient;
+import com.derahh.retrofitbasic.services.GetService;
+
+import java.util.List;
+
+import retrofit2.Call;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CustomAdapter adapter;
+    private RecyclerView recyclerView;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Loading....");
+        progressDialog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        GetService service = ApiClient.getRetrofitInstance().create(GetService.class);
+        Call<List<PhotoData>> call;
     }
 }
