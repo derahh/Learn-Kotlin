3d92276cfef0fc36b300f63d800ad00b09645f62
Dedi Rahmansah <derah666@gmail.com>
2020-01-08 14:28:09 +0700

    Update APIService.java

--- RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/Service/APIService.java
+++ RetrofitBasic-Java/app/src/main/java/com/derahh/retrofitbasicjava/Service/APIService.java
@@ -1,4 +1,14 @@
 package com.derahh.retrofitbasicjava.Service;
 
+import com.derahh.retrofitbasicjava.model.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface APIService {
+
+    @GET("users")
+    Call<List<User>> fetchAllUser();
 }
