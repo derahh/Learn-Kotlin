39964928645631e05308ae54d1356d8147c83ee8
Dedi Rahmansah <derah666@gmail.com>
2020-01-14 16:53:10 +0700

    Update MainActivity.java

--- ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivity.java
+++ ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivity.java
@@ -7,10 +7,13 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,11 +36,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        TextView tvNumber = findViewById(R.id.txt_number);
-//        MainActivityViewModel myData = new MainActivityViewModel();
-        MainActivityViewModel viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
-        String myRandomNumber = viewModel.getNumber();
-        tvNumber.setText(myRandomNumber);
+        final TextView tvNumber = findViewById(R.id.txt_number);
+        Button btnRandom = findViewById(R.id.btn_random);
+
+        final MainActivityViewModel viewModel = ViewModelProviders.of(this).get(MainActivityViewModel.class);
+        final LiveData<String> myRandomNumber = viewModel.getNumber();
+
+        myRandomNumber.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tvNumber.setText(s);
+                Log.i(TAG, "Data Updated in UI");
+            }
+        });
+
+        btnRandom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.cretaeNumber();
+            }
+        });
+
         Log.i(TAG, "Random Number Set");
     }
 }
