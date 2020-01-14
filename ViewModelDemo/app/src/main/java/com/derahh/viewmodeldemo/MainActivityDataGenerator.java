a5431e21c5ccc2e84806594474ed9d8c82689a28
Dedi Rahmansah <derah666@gmail.com>
2020-01-14 14:43:14 +0700

    Update MainActivityDataGenerator.java

--- ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivityDataGenerator.java
+++ ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivityDataGenerator.java
@@ -1,4 +1,25 @@
 package com.derahh.viewmodeldemo;
 
+import android.util.Log;
+
+import java.util.Random;
+
 public class MainActivityDataGenerator {
+
+    private String TAG = this.getClass().getSimpleName();
+    private String myRandomNumber;
+
+    public String getNumber() {
+        Log.i(TAG, "Get number");
+        if (myRandomNumber == null) {
+            cretaeNumber();
+        }
+        return myRandomNumber;
+    }
+
+    private void cretaeNumber() {
+        Log.i(TAG, "Create new number");
+        Random random = new Random();
+        myRandomNumber = "Number: " +(random.nextInt(10 - 1) + 1);
+    }
 }
