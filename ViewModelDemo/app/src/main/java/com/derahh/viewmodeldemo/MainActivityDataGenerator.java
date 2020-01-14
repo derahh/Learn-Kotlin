b941f75df1b0e69b9cadb579183e8c3a6f319881
Dedi Rahmansah <derah666@gmail.com>
2020-01-14 15:10:22 +0700

    Update MainActivityDataGenerator.java

--- ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivityDataGenerator.java
+++ ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivityDataGenerator.java
@@ -2,9 +2,11 @@ package com.derahh.viewmodeldemo;
 
 import android.util.Log;
 
+import androidx.lifecycle.ViewModel;
+
 import java.util.Random;
 
-public class MainActivityDataGenerator {
+public class MainActivityDataGenerator extends ViewModel {
 
     private String TAG = this.getClass().getSimpleName();
     private String myRandomNumber;
@@ -21,5 +23,11 @@ public class MainActivityDataGenerator {
         Log.i(TAG, "Create new number");
         Random random = new Random();
         myRandomNumber = "Number: " +(random.nextInt(10 - 1) + 1);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.i(TAG, "ViewModel Destroyed");
     }
 }
