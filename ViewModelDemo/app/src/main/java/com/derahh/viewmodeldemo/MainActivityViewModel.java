9c2acd3b552b44a5575f9431e7dca6c7825a4261
Dedi Rahmansah <derah666@gmail.com>
2020-01-14 16:53:02 +0700

    Update MainActivityViewModel.java

--- ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivityViewModel.java
+++ ViewModelDemo/app/src/main/java/com/derahh/viewmodeldemo/MainActivityViewModel.java
@@ -2,6 +2,7 @@ package com.derahh.viewmodeldemo;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.Random;
@@ -9,20 +10,21 @@ import java.util.Random;
 public class MainActivityViewModel extends ViewModel {
 
     private String TAG = this.getClass().getSimpleName();
-    private String myRandomNumber;
+    private MutableLiveData<String> myRandomNumber;
 
-    public String getNumber() {
+    public MutableLiveData<String> getNumber() {
         Log.i(TAG, "Get number");
         if (myRandomNumber == null) {
+            myRandomNumber = new MutableLiveData<>();
             cretaeNumber();
         }
         return myRandomNumber;
     }
 
-    private void cretaeNumber() {
+    public void cretaeNumber() {
         Log.i(TAG, "Create new number");
         Random random = new Random();
-        myRandomNumber = "Number: " +(random.nextInt(10 - 1) + 1);
+        myRandomNumber.setValue("Number: " +(random.nextInt(10 - 1) + 1));
     }
 
     @Override
