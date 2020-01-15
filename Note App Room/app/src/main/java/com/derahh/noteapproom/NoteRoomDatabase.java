42a2fa516dce0ffe88e306012bbb20e496491ee6
Dedi Rahmansah <derah666@gmail.com>
2020-01-15 10:02:11 +0700

    Update NoteRoomDatabase.java

--- Note App Room/app/src/main/java/com/derahh/noteapproom/NoteRoomDatabase.java
+++ Note App Room/app/src/main/java/com/derahh/noteapproom/NoteRoomDatabase.java
@@ -1,6 +1,28 @@
 package com.derahh.noteapproom;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+@Database(entities = Note.class, version = 1)
 public abstract class NoteRoomDatabase extends RoomDatabase {
+
+    public abstract NoteDao noteDao();
+
+    private static volatile NoteRoomDatabase mInstance;
+
+    static NoteRoomDatabase getDatabase(final Context context) {
+        if (mInstance == null) {
+            synchronized (NoteRoomDatabase.class) {
+                if (mInstance == null) {
+                    mInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            NoteRoomDatabase.class, "note_database")
+                            .build();
+                }
+            }
+        }
+        return mInstance;
+    }
 }
