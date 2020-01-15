2f4deb355d49d45df49e11941ab8b7dc59c9bb87
Dedi Rahmansah <derah666@gmail.com>
2020-01-15 10:58:02 +0700

    Update MainActivity.java

--- Note App Room/app/src/main/java/com/derahh/noteapproom/MainActivity.java
+++ Note App Room/app/src/main/java/com/derahh/noteapproom/MainActivity.java
@@ -1,10 +1,11 @@
 package com.derahh.noteapproom;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
@@ -12,9 +13,13 @@ import androidx.lifecycle.ViewModelProviders;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NEW_NOTE_ACTIVITY_REQUEST_CODE = 1;
     private String TAG = this.getClass().getSimpleName();
     private NoteViewModel noteViewModel;
 
@@ -29,12 +34,29 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, NewNoteActivity.class);
+                startActivityForResult(intent, NEW_NOTE_ACTIVITY_REQUEST_CODE);
             }
         });
 
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_NOTE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            //code to insert note
+            final String note_id = UUID.randomUUID().toString();
+            Note note = new Note(note_id, data.getStringExtra(NewNoteActivity.NOTE_ADDED));
+            noteViewModel.insert(note);
+
+            Toast.makeText(getApplicationContext(), R.string.saved, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.not_saved, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
