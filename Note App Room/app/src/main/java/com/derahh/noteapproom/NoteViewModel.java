aa3b2753c163be070142881462f36127eb70fbc8
Dedi Rahmansah <derah666@gmail.com>
2020-01-15 10:57:46 +0700

    Update NoteViewModel.java

--- Note App Room/app/src/main/java/com/derahh/noteapproom/NoteViewModel.java
+++ Note App Room/app/src/main/java/com/derahh/noteapproom/NoteViewModel.java
@@ -1,6 +1,7 @@
 package com.derahh.noteapproom;
 
 import android.app.Application;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -9,14 +10,38 @@ import androidx.lifecycle.AndroidViewModel;
 public class NoteViewModel  extends AndroidViewModel {
 
     private String TAG = this.getClass().getSimpleName();
+    private NoteDao noteDao;
+    private NoteRoomDatabase noteDB;
 
     public NoteViewModel(@NonNull Application application) {
         super(application);
+
+        noteDB = NoteRoomDatabase.getDatabase(application);
+        noteDao = noteDB.noteDao();
+    }
+
+    public void insert(Note note) {
+        new InsertAsyncTask(noteDao).execute(note);
     }
 
     @Override
     protected void onCleared() {
         super.onCleared();
         Log.i(TAG, "ViewModel Destroyed");
+    }
+
+    private class InsertAsyncTask extends AsyncTask<Note, Void, Void> {
+
+        NoteDao mNoteDao;
+
+        public InsertAsyncTask(NoteDao noteDao) {
+            this.mNoteDao = noteDao;
+        }
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            mNoteDao.insert(notes[0]);
+            return null;
+        }
     }
 }
