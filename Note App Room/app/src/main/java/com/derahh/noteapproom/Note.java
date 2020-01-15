d33518b981cd8713f20f0d4df435f053ba94fd60
Dedi Rahmansah <derah666@gmail.com>
2020-01-15 10:57:28 +0700

    Update Note.java

--- Note App Room/app/src/main/java/com/derahh/noteapproom/Note.java
+++ Note App Room/app/src/main/java/com/derahh/noteapproom/Note.java
@@ -1,7 +1,33 @@
 package com.derahh.noteapproom;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity(tableName = "notes")
 public class Note {
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    @NonNull
+    public String getNote() {
+        return mNote;
+    }
+
+    @PrimaryKey
+    @NonNull
+    private String id;
+
+    @NonNull
+    @ColumnInfo(name = "note")
+    private String mNote;
+
+    public Note(String id, String note){
+        this.id = id;
+        this.mNote = note;
+    }
 }
