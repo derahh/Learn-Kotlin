192602d15c82f9fc873f875d4375aa1d0d521ee9
Dedi Rahmansah <derah666@gmail.com>
2020-01-15 09:58:18 +0700

    Update NoteViewModel.java

--- Note App Room/app/src/main/java/com/derahh/noteapproom/NoteViewModel.java
+++ Note App Room/app/src/main/java/com/derahh/noteapproom/NoteViewModel.java
@@ -1,4 +1,22 @@
 package com.derahh.noteapproom;
 
-public class NoteViewModel {
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class NoteViewModel  extends AndroidViewModel {
+
+    private String TAG = this.getClass().getSimpleName();
+
+    public NoteViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.i(TAG, "ViewModel Destroyed");
+    }
 }
