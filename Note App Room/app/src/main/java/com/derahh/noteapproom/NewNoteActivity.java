89ff2a962df14eec5084a4557655c9edc6dc4b7b
Dedi Rahmansah <derah666@gmail.com>
2020-01-15 10:58:06 +0700

    Update NewNoteActivity.java

--- Note App Room/app/src/main/java/com/derahh/noteapproom/NewNoteActivity.java
+++ Note App Room/app/src/main/java/com/derahh/noteapproom/NewNoteActivity.java
@@ -2,13 +2,43 @@ package com.derahh.noteapproom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewNoteActivity extends AppCompatActivity {
+
+    public final static String NOTE_ADDED = "new_note";
+
+    private EditText edtNewNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_note);
+
+        edtNewNote = findViewById(R.id.edt_new_note);
+
+        Button btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent resultIntent = new Intent();
+
+                if (TextUtils.isEmpty(edtNewNote.getText())) {
+                    setResult(RESULT_CANCELED, resultIntent);
+                } else {
+                    String note = edtNewNote.getText().toString();
+                    resultIntent.putExtra(NOTE_ADDED, note);
+                    setResult(RESULT_OK, resultIntent);
+                }
+
+                finish();
+            }
+        });
     }
 }
