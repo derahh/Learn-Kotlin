b452601f006cebb9dedbee11a1b6c972a2eb3bfc
Dedi Rahmansah <derah666@gmail.com>
2020-01-29 13:49:07 +0700

    JSON Parsing

--- My Volley JSON Parsing/app/src/main/java/com/derahh/myvolleyjsonparsing/MainActivity.java
+++ My Volley JSON Parsing/app/src/main/java/com/derahh/myvolleyjsonparsing/MainActivity.java
@@ -1,14 +1,80 @@
 package com.derahh.myvolleyjsonparsing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecycler;
+    private Adapter adapter;
+    private ArrayList<Model> listData;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecycler = findViewById(R.id.recylcer_view);
+        mRecycler.setHasFixedSize(true);
+        mRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        listData = new ArrayList<>();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        parseJSON();
+    }
+
+    private void parseJSON() {
+        String url = "https://pixabay.com/api/?key=5303976-fd6581ad4ac165d1b75cc15b3&q=kitten&image_type=photo&pretty=true";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("hits");
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject hit = jsonArray.getJSONObject(i);
+
+                                String creatorName = hit.getString("user");
+                                String imageUrl = hit.getString("webformatURL");
+                                int likeCOunt = hit.getInt("likes");
+
+                                listData.add(new Model(imageUrl, creatorName, likeCOunt));
+                            }
+
+                            adapter = new Adapter(MainActivity.this, listData);
+                            mRecycler.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mRequestQueue.add(request);
     }
 }
