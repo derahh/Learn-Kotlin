f19654af9ec5f95b0b5a4483cfab2941bcfd8e8a
Dedi Rahmansah <derah666@gmail.com>
2020-02-22 08:35:15 +0700

    modifed qisquschattest

--- QiscusChatTest/app/src/main/java/com/derahh/qiscuschattest/MainActivity.java
+++ QiscusChatTest/app/src/main/java/com/derahh/qiscuschattest/MainActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.qiscus.sdk.chat.core.QiscusCore;
+import com.qiscus.sdk.chat.core.data.model.QiscusAccount;
+import com.qiscus.sdk.chat.core.data.remote.QiscusApi;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUser();
+    }
+
+    private void setUser() {
+        QiscusCore.setUser("derah666@gmail.com", "tunggudulu")
+                .withUsername("Dedi Rahmansah")
+                //.withAvatarUrl(avatarUrl)
+                //.withExtras(extras)
+                .save(new QiscusCore.SetUserListener() {
+                    @Override
+                    public void onSuccess(QiscusAccount qiscusAccount) {
+                        //on success
+                        openChat();
+                    }
+                    @Override
+                    public void onError(Throwable throwable) {
+                        //on error
+                    }
+                });
+    }
+
+    private void openChat() {
+
     }
 }
